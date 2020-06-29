25323376af17176c545f665d53cde848502b5e8f
Olamide Oladinni <olajide1987@gmail.com>
2020-06-29 18:30:08 -0500

    auth-works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,33 @@
 import '../styles/main.scss';
-import 'bootstrap';
 
-console.error('this works');
+import firebase from 'firebase/app';
+import apiKeys from './helpers/data/apiKeys.json';
+
+import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
+import auth from './components/auth/auth';
+
+const authDiv = $('#auth');
+const logoutButton = $('#navbar-logout-button');
+const loginButton = $('#google-auth');
+
+const showLoggedInView = () => {
+  authDiv.addClass('hide');
+  logoutButton.removeClass('hide');
+  loginButton.addClass('hide');
+};
+
+const showLoggedOutView = () => {
+  authDiv.removeClass('hide');
+  logoutButton.addClass('hide');
+  loginButton.removeClass('hide');
+};
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus(showLoggedInView, showLoggedOutView);
+  auth.loginButton();
+  myNavbar.logoutEvent();
+};
+
+init();
